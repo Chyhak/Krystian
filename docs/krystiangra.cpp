fb37595ffa534c865019c92f483ebae438bfd5c2
shtyq <58339222+shtyq@users.noreply.github.com>
2019-11-30 12:20:03 +0100

    Add files via upload

---
+++ docs/krystiangra.cpp
@@ -0,0 +1,417 @@
+#include <iostream>
+#include <conio.h>
+#include <cstdlib>
+using namespace std;
+
+void intro()
+{
+	system("cls");
+	cout<<"Rok 2019 Ziemia godzina 16:20"<<endl;
+	cout<<"4 dni do imprezy urodzinowej"<<endl;
+	_getch();
+	cout<<"-”*ziew*”"<<endl;
+	_getch();
+	cout<<"”Ile mo¿na graæ w te lige...”"<<endl;
+	_getch();
+	cout<<"”Dobra ch³opaki ja nie gram ju¿ nexta ide coœ zjeœæ grajcie beze mnie”"<<endl;
+	_getch();
+	cout<<"-”No spoko, narazie” -”No elo”"<<endl;
+	_getch();
+	cout<<"*WSTAJE OD KOMPA*"<<endl;
+	_getch();
+	cout<<"-”Odlalbym siê jeszcze, mo¿e skocze jeszcze szybko do kibla”"<<endl;
+	_getch();
+	cout<<"POJSC DO KLOPA? (Y/N)"<<endl;
+}
+
+void kibel()
+{
+	system("cls");
+	cout<<"*ziip*"<<endl;
+	_getch();
+	cout<<"-”ahhh co za ulg-”"<<endl;
+	_getch();
+}
+
+void kuchnia()
+{
+	system("cls");
+	cout<<"*Otwiera drzwi lodówki*"<<endl;
+	_getch();
+	cout<<"“Hmmm co by tu dzisiaj... AHA! Jest jeszcze pizza z wczoraj”"<<endl;
+	_getch();
+	cout<<"*OMNOMNOM-* *KASZLE*"<<endl;
+	_getch();
+}
+
+void alien()
+{
+	cout<<"”AAAA CO TO KURWA JEST”"<<endl;
+	_getch();
+	cout<<"-”Witaj Krystianie.”"<<endl;
+	_getch();
+	cout<<"“Rozumiemy twa bojazn obecna koniuktura wydarzen ale nie lekaj siê, mamy szla-”"<<endl;
+	_getch();
+	cout<<"-”Zamknac pyski, nie widzicie, ze oglosili nowe skiny w lolu??” *patrzy w telefon*"<<endl;
+	_getch();
+	cout<<"-”Niew¹tpliwie, tak jak nasza maszyna przewidziala, wszystko idzie zgodnie z planem”"<<endl;
+	_getch();
+	cout<<"-”Ale kim wy do cholery jestescie? To jakis jebany prank?”"<<endl;
+	_getch();
+	cout<<"-”Jestesmy Gevanami. Nasza planeta to CoRoT-7b z gwiazdozbioru Jednoro¿ca, jestesmy oddaleni od ziemi o oko³o 490 lat swietl-”"<<endl;
+	_getch();
+	cout<<"-”Ryj tam ju¿, wypierdalac z mojego domu bo psem poszcz-”"<<endl;
+	_getch();
+	cout<<"-”Twoja impreza urodzinowa siê nie odbedzie i bêdzie kiepska”"<<endl;
+	_getch();
+	cout<<"-”Co kurwa?? O czym wy gadacie”"<<endl;
+	_getch();
+	cout<<"-”Wlasnie tak. Przyszlismy  Cie ostrzec a wlasciwie to zaoferowac oferte pomocy, w zamian za uratowanie jakze waznej dla ciebie imprezy”"<<endl;
+	_getch();
+	cout<<"“Wiemy, ze to mo¿e byæ dla ciebie teraz za wiele ale im mniej czasu teraz stracimy tym wieksze prawdopodobienstwo sukcesu twojej misji”"<<endl;
+	_getch();
+	cout<<"-”Dosyc tego gadania wypier-” *bzzz*"<<endl;
+	_getch();
+	cout<<"“OUGH KURWA”"<<endl;
+	_getch();
+	cout<<"*MDLEJE*"<<endl;
+	_getch();
+	cout<<"-”ahhh no to po prostu kurwa by³o nieuniknione”"<<endl;
+	_getch();
+	cout<<"“debil nawet nie dal sobie nic wytlumaczyc i wysluchac do konca”"<<endl;
+	_getch();
+	cout<<"“zabierajcie go na statek, zrobi to co musi czy tego chce czy nie”"<<endl;
+	_getch();
+}
+
+void pobudka()
+{
+	system("cls");
+	cout<<"Rok 2019 Statek matka, Uk³ad s³oneczny 249 lat œwietlnych od ziemi godzina 4:20 czasu ziemskiego"<<endl;
+	cout<<"3 dni do imprezy urodzinowej"<<endl;
+	_getch();
+	cout<<"*Krystianowi powoli wraca swiadomosc*"<<endl;
+	_getch();
+	cout<<"-“[...] to jest nie do pomyslenia ze z posrod wszystkich wyrocznia wybrala akurat tego idiote!”"<<endl;
+	_getch();
+	cout<<"-”ugwan yex yex hstza”"<<endl;
+	_getch();
+	cout<<"-”zgadzam siê z UlanGiem, jeœli wyrocznia tak wybrala to znaczy ze przepowiednia siê spelni i ten prymitywny ziemianin uchroni nas przed zagla-”"<<endl;
+	_getch();
+	cout<<"-”Ughhhh”"<<endl;
+	_getch();
+	cout<<"-”Szybko, odzyskuje swiadomosc! Niech Az’mang przybedzie!”"<<endl;
+	_getch();
+	cout<<"-”Gdzie ja kurwa jestem...?”"<<endl;
+	_getch();
+	cout<<"“Moja glowa...”"<<endl;
+	_getch();
+	cout<<"-”Jam jest wszechpotezny Az’mang! Przywodzca miedzygalaktycznej wspolnoty kosmicznej i stworca statku Matka”"<<endl;
+	_getch();
+	cout<<"“Nie lekaj siê mlody przybyszu z planety zwanej ziemia”"<<endl;
+	_getch();
+	cout<<"“W imieniu moim bardzo przepraszam ze drastyczne kroki podjete przez moja zaloge aby cie tu sprowadzic”"<<endl;
+	_getch();
+	cout<<"“Poniesli ju¿ oni konsekwencje za swój haniebny czyn”"<<endl;
+	_getch();
+	cout<<"-“Co to za miejsce? I dlaczego tu jestem?”"<<endl;
+	_getch();
+	cout<<"-”Tak jak ju¿ powiedzialem, znajdujemy siê w przestrzeniu kosmicznej na moim statku wyposazonym w najlepsza technologie w calej galaktyce”"<<endl;
+	_getch();
+	cout<<"“Twoja obecnosc jest bardzo wazna dla wszystkich, zarówno jak i dla nas jak i dla ciebie”"<<endl;
+	_getch();
+	cout<<"“Cala Droga mleczna jest w zagrozeniu, nieznane nikomu czasteczki galaktyczne prowadza do rozpadu ciemnej materii która odpowiada za 27% bilansu masy-energii wszechswiata”"<<endl;
+	_getch();
+	cout<<"“Bez niej jestesmy zgubieni, nawet nie jestem wstanie sobie wyobrazic co mo¿e siê wydarzyc”"<<endl;
+	_getch();
+	cout<<"-”NO DOBRA. Tylko jeszcze raz po kiego grzyba ja wam jestem potrzebny??”"<<endl;
+	_getch();
+	cout<<"-”Bylismy bezradni, udalismy siê wiec o pomoc do najswietrzej wyroczni aby dala nam jakies wskazowki”"<<endl;
+	_getch();
+	cout<<"“Wskazala na Ciebie, tylko ty jestes w stanie uratowac swiat i tym samym ocalic swiat!”"<<endl;
+	_getch();
+	cout<<"-”EEEE jakos mnie to nie przekonuje”"<<endl;
+	_getch();
+	cout<<"-”Bez ciemnej materii nie bêdzie swiata a bez swiata nie bêdzie twojej osiemnastki”"<<endl;
+	_getch();
+	cout<<"..."<<endl;
+	_getch();
+	cout<<"-”Dobra to co mam zrobic?”"<<endl;
+	_getch();
+	cout<<"-”Najwspanialsza wyrocznia wspomniala o 3 zoltych magicznych pudlach”"<<endl;
+	_getch();
+	cout<<"“Porozrzucane sa one na roznych planetach w naszej galaktyce”"<<endl;
+	_getch();
+	cout<<"“Wlasnie kierujemy siê do pierwszej”"<<endl;
+	_getch();
+	cout<<"“Musisz je wszystkie zdobyc i zlaczyc w calosc”"<<endl;
+	_getch();
+	cout<<"“Jestes wybrancem”"<<endl;
+	_getch();
+	system("cls");
+	cout<<"..."<<endl;
+	_getch();
+	cout<<"-”A jak mam niby zebrac to wszystko? Jestem w swoich zwyklych ubranich nie mam zadnego kombinezonu ani nic”"<<endl;
+	_getch();
+	cout<<"-”O to siê nie muszisz martwic”"<<endl;
+	_getch();
+	cout<<"“Nie wiedziales tego ale twoje cialo jest odporne na brak tlenu i proznie”"<<endl;
+	_getch();
+	cout<<"-”CO KURWA??”"<<endl;
+	_getch();
+	cout<<"“Musisz sobie zaufac a przede wszystkim musisz zaufac najwspanialszej wyroczni”"<<endl;
+	_getch();
+	cout<<"-”Nie ma mowy! Nigdzie siê nie wybieram bez odpowiednich zabezpie-”"<<endl;
+	_getch();
+	cout<<"-”Alez gdzie moje maniery, zapewne jestes glodny. Mamy maszyne do tworzenia jedzenia z kazdego zakatka swiata, w tym oczywiœcie Ziemi”"<<endl;
+	_getch();
+	cout<<"“Masz ochote na jakas Ziemska potrawe?”"<<endl;
+	_getch();
+}
+
+void najedzony()
+{
+	cout<<"-“Nie jestem glodny, by³em w trakcie jedzenia pizzy kiedy twoi ludzie na mnie napadli”"<<endl;
+	_getch();
+	cout<<"“Za to musze siê odlac bo zaraz siê poszcze w gacie...”"<<endl;
+	_getch();
+	cout<<"-”Oczywiscie. Tutaj jest specjalistyczny sprzet który automatycznie niweluje gromadzenie siê moczu i kalu w organizmie, weŸ to proszê przyda cie siê w podrozy”"<<endl;
+	_getch();
+}
+
+void nienajedzony()
+{
+	cout<<"-”Jestem cholernie glodny chcialem cos zjesc w domu ale twoi ludzie uniemozliwili mi to...”"<<endl;
+	_getch();
+	cout<<"-“Oczywiscie. Tutaj jest specjalistyczny sprzet który automatycznie niweluje glod, ale na poczatku zjedz tego ziemskiego kebaba bo sprzet musi przystosowac siê do warunkow aby zaczal dzialac”"<<endl;
+	_getch();
+	cout<<"-”oooo kebsik zajebiscie”"<<endl;
+	_getch();
+	cout<<"-”Z baranina?” (Y/N)"<<endl;
+}
+
+void planeta()
+{
+	system("cls");
+	cout<<"Rok 2019 Statek matka, Uk³ad s³oneczny 250 lat œwietlnych od ziemi godzina 4:57 czasu ziemskiego"<<endl;
+	cout<<"3 pude³ka do zebrania"<<endl;
+	_getch();
+	cout<<"-“Nareszcie docieramy na miejsce kapitanie!”"<<endl;
+	_getch();
+	cout<<"-”Kapitalnie! Zawo³aj Krystiana”"<<endl;
+	_getch();
+	cout<<"“Czujê, ¿e tajemnicze pude³ko jest w pobli¿u...”"<<endl;
+	_getch();
+	cout<<"-”Kapitanie oto Krystian, wydajê mi siê ze przerwaliœmy mu proces ejakula-”"<<endl;
+	getch();
+	cout<<"-”Zamknijcie mordy wkurwia mnie ju¿ ta ca³a podró¿ tym statkiem”"<<endl;
+	getch();
+	cout<<"“Krêci mi siê w g³owie i jest mi niedobrze”"<<endl;
+	_getch();
+	cout<<"-”Niew¹tpliwie, czy jesteœ gotowy na skok po pierwsze pude³ko?”"<<endl;
+	_getch();
+	cout<<"-”Tak.. Nie czekaj nie, kurwa”"<<endl;
+	_getch();
+	cout<<"-”Szybka odpowiedŸ tak czy nie” (Y/N)"<<endl;
+}
+
+void planetapartdwa()
+{
+	cout<<"*HTSZZZZ*"<<endl;
+	_getch();
+	cout<<"-”CO ALE TO JUZ?! KURWA KURWA KURWA!!”"<<endl;
+	_getch();
+	cout<<"*siuuup*"<<endl;
+	_getch();
+	cout<<"“NIE MOGÊ ODDYCHAC!!! GHAAG”"<<endl;
+	_getch();
+	cout<<"“Hej zaraz nie jest tak Ÿle w tej przestrzeni kosmicznej”"<<endl;
+	_getch();
+	cout<<"“Chyba nawet mogê siê tutaj swobodnie poruszaæ tak jak to mówi³ Az’mang”"<<endl;
+	_getch();
+	cout<<"“Trochê jak w jakiejœ cieczy”"<<endl;
+	_getch();
+	cout<<"“I na chuj by³y te wszystkie lekcje fizyki jak ja wszystkiemu teraz zaprzeczam...”"<<endl;
+	_getch();
+	cout<<"..."<<endl;
+	_getch();
+	cout<<"“Niewa¿ne”"<<endl;
+	_getch();
+	cout<<"“Coœ podejrzanie wygl¹da to pojedyncze stoisko zawieszone na atmosferze tej planety...”"<<endl;
+	_getch();
+	cout<<"*siuup*"<<endl;
+	_getch();
+	cout<<"-“Witaj jak¿e dziwny podró¿niku z zaœwiatów Gurlp !!! Czy chcesz zagraæ w moj¹ grê?”"<<endl;
+	_getch();
+	cout<<"-”Nie dziêki. Jestem tu z misj¹”"<<endl;
+	_getch();
+	cout<<"“Muszê odzyskaæ ¿ó³te pude³ko, nie widzia³eœ mo¿e takowego?”"<<endl;
+	_getch();
+	cout<<"-”Gurlp hmmm ¿ó³te pude³ko pomyœlmy... A by³o ono MAGICZNE??”"<<endl;
+	_getch();
+	cout<<"-”Tak, podobno znajdujê siê na tej planecie widzia³eœ czy nie?”"<<endl;
+	_getch();
+	cout<<"-”Grulp, hmmmm mo¿e tak, mo¿e nie...”"<<endl;
+	_getch();
+	cout<<"*£APIESZ GO ZA FRAKI*"<<endl;
+	_getch();
+	cout<<"-”Dobra kurwa gadaj czy widzia³eœ to pud³o bo nie mam czasu na twoje zabawy”"<<endl;
+	_getch();
+	cout<<"-“Khe kekekekeke xD Grulp”"<<endl;
+	_getch();
+	cout<<"“Tak siê sk³ada, ¿e mam jedno takie Grulp ¿ó³te pude³ko”"<<endl;
+	_getch();
+	cout<<"“Czy mówisz o nim?” *pokazujê*"<<endl;
+	_getch();
+	cout<<"-”Zaraz... Tak... Coœ mi podpowiada, ¿e to w³aœnie jego szukam”"<<endl;
+	_getch();
+	cout<<"-”Grulp Wspaniale! Mogê Ci je zaoferowaæ je¿eli wygrasz ze mn¹ w moj¹ grê”"<<endl;
+	_getch();
+	cout<<"“Czy teraz Ciê przekona³em Grulp do zagrania w moj¹ grê??” (Y/N)"<<endl;
+}
+
+void gra()
+{
+	system("cls");
+	cout<<"-”Zasady s¹ bardzo proste... Myœlê o liczbie od 1 do 5”"<<endl;
+	_getch();
+	cout<<"“Jeœli zgadniesz Gurlp o jakiej liczbie myœlê dostaniesz pude³ko!”"<<endl;
+	_getch();
+	cout<<"-”I tyle?”"<<endl;
+	_getch();
+	cout<<"-”Tak!!!! Gurlp ale uwa¿aj masz tylko 1000 prób, jeœli nie zgadniesz to bêdê Ciê musia³ zabiæ i zniszczyæ to pude³ko khe kekekeke xD Gurlp”"<<endl;
+	_getch();
+	cout<<"-*Sigh*"<<endl;
+	_getch();
+	cout<<"-“Gurlp Wpaniale!! A wiêc zaczynajmy!!”"<<endl;
+	_getch();
+	cout<<"“Wybierz dowoln¹ liczbê, przypominam, ¿e myœlê o liczbie od 1 do 5 Gurlp...”"<<endl;
+}
+
+void wygrana()
+{
+	cout<<"“CO??? GURLP GURLP Nie mo¿liwe, ¿e uda³o Ci siê tak szybko odgadn¹æ moj¹ liczbê!!!”"<<endl;
+	_getch();
+	cout<<"-”Wygra³em wiêc zamknij ³eb i dawaj pud-”"<<endl;
+	_getch();
+	cout<<"“Co do kurwy”"<<endl;
+	_getch();
+	cout<<"-”Eh? Gurlp”"<<endl;
+	_getch();
+	cout<<"*Widzisz 2 ¿ó³te pude³ka za lad¹*"<<endl;
+	_getch();
+	cout<<"-“Jak to kurwa masz 2 ¿ó³te pud³a a nie 1?”"<<endl;
+	_getch();
+	cout<<"“DAWAJ JE KURWA”"<<endl;
+	_getch();
+	cout<<"-”Gurlp khe kekekeke xD umawialiœmy siê na jedn-” *JEB*"<<endl;
+	_getch();
+	cout<<"-”Dawaj te pud³a i nie gadaj idioto wygra³em to wygra³em”"<<endl;
+	_getch();
+	cout<<"*Dodano dwa pud³a do ekwipunku*"<<endl;
+	_getch();
+	cout<<"“Dobra czas wracaæ na statek”"<<endl;
+	_getch();
+	cout<<"*siuuup*"<<endl;
+	_getch();
+	cout<<"*siuup*"<<endl;
+	_getch();
+}
+
+int main()
+{
+	setlocale(LC_CTYPE, "Polish");
+	char wybor;
+	float szczanie, jedzenie, gral;
+	int rand();
+	szczanie = 0;
+	jedzenie = 0;
+	intro();
+	start:
+	cin>>wybor;
+	switch(wybor)
+	{
+		case 'y' :
+			kibel();
+			szczanie++;
+			break;
+		case 'n' :
+			kuchnia();
+			jedzenie++;
+			break;
+		default :
+			goto start;
+	}
+	alien();
+	pobudka();
+	if(jedzenie==1)
+	{
+		najedzony();
+	}
+	if(jedzenie==0)
+	{
+		nienajedzony();
+		cin>>wybor;
+		switch(wybor)
+		{
+			case 'y' :
+				cout<<"-”Kozaczek, proszê bardzo i smacznego”"<<endl;
+				_getch();
+				break;
+			case 'n' :
+				cout<<"-”Meh, trzymaj”"<<endl;
+				_getch();
+				break;
+		}
+	}
+	planet:
+	planeta();
+	cin>>wybor;
+	switch(wybor)
+	{
+		case 'y' :
+			cout<<"”Doskonale a wiêc otworzyæ kabinê”"<<endl;
+			_getch();
+			break;
+		case 'n' :
+			cout<<"”Za póŸno, otwieraæ kabinê”"<<endl;
+			_getch();
+			break;
+	}
+	planetapartdwa();
+	cin>>wybor;
+	switch(wybor)
+	{
+		case 'y' :
+			cout<<"“Gurlp Wspaniale! A wiêc przejdŸmy do zasad tej gry...”"<<endl;
+			_getch();
+			cout<<"-”Ehhh”"<<endl;
+			_getch();
+			break;
+		case 'n' :
+			cout<<"“GURLP... NO TO WYPIERDALAJ NIE BÊDZIE ¯ADNEGO ¯Ó£TEGO PUD£A”"<<endl;
+			_getch();
+			cout<<"-”Dobra dobra czekaj... Ju¿ zagram ¿artowa³em tylko... :P”"<<endl;
+			_getch();
+			cout<<"“Co mam robiæ?”"<<endl;
+			_getch();
+			break;
+	}
+	gra();
+	gram:
+	cin>>gral;
+	if(gral!=(rand()%4+1))
+	{
+		cout<<"“Gurlp!!! Zla liczba spróbuj jeszcze raz!!1 khe kekeke xD”"<<endl;
+		_getch();
+		cout<<"-”kurw...”"<<endl;
+		_getch();
+		cout<<"-“Spróbuj jeszcze raz”"<<endl;
+		_getch();
+		goto gram;
+	}
+	else
+	{
+		wygrana();
+	}
+}
